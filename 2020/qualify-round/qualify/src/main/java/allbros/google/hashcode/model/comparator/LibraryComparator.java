602b0e40bc770a2f4da6358eed928473ae74db65
Alejandro González Santiago <agonsant@everis.com>
2020-02-20 19:31:57 +0100

    typo fixed

--- 2020/qualify-round/qualify/src/main/java/allbros/google/hashcode/model/comparator/LibraryComparator.java
+++ 2020/qualify-round/qualify/src/main/java/allbros/google/hashcode/model/comparator/LibraryComparator.java
@@ -1,8 +1,8 @@
-package allbros.google.hashcode.model.compartor;
+package allbros.google.hashcode.model.comparator;
 
 import allbros.google.hashcode.model.Library;
 
-public class LibraryCompartor implements Comparable<Library> {
+public class LibraryComparator implements Comparable<Library> {
     @Override
     public int compareTo(Library library) {
         return 0;
