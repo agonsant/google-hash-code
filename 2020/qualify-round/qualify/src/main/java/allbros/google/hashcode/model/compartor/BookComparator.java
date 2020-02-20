366641493991940d5a6db0f725299e634631488a
Carlos A. González Santiago <twigy11@gmail.com>
2020-02-20 19:31:21 +0100

    add structure comparator

---
+++ 2020/qualify-round/qualify/src/main/java/allbros/google/hashcode/model/compartor/BookComparator.java
@@ -0,0 +1,10 @@
+package allbros.google.hashcode.model.compartor;
+
+import allbros.google.hashcode.model.Book;
+
+public class BookComparator implements Comparable<Book> {
+    @Override
+    public int compareTo(Book book) {
+        return 0;
+    }
+}
---
+++ 2020/qualify-round/qualify/src/main/java/allbros/google/hashcode/model/compartor/LibraryCompartor.java
@@ -0,0 +1,10 @@
+package allbros.google.hashcode.model.compartor;
+
+import allbros.google.hashcode.model.Library;
+
+public class LibraryCompartor implements Comparable<Library> {
+    @Override
+    public int compareTo(Library library) {
+        return 0;
+    }
+}
